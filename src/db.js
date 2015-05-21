a6fee18f258d35dcad408ff71fa4b521b6a76f46
Steven Vergenz <steve.vergenz.ctr@adlnet.gov>
2015-05-21 11:28:10 -0400

    Fixed SQL typo

--- src/db.js
+++ src/db.js
@@ -38,7 +38,7 @@ function initializeDatabase(path)
 		'	asset INT UNSIGNED,'+
 
 		'	PRIMARY KEY(id,key),'+
-		'	FOREIGN KEY (id) REFERENCES Assets(id) ON DELETE CASCADE'+
+		'	FOREIGN KEY (id) REFERENCES Assets(id) ON DELETE CASCADE,'+
 		'	FOREIGN KEY (asset) REFERENCES Assets(id) ON DELETE SET NULL'+
 		')', handleError('Failed to create metadata table'))
 
